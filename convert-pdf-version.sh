ca44a131f0b0bd8c8591f709d91a1571c4d4d863
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-07-31 10:29:52 -0300

    Add progress information on screen

--- convert-pdf-version.sh
+++ convert-pdf-version.sh
@@ -5,5 +5,8 @@ cd $1
 
 for i in *
 do
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$FILEPATH/$2$i ./$i
+  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$FILEPATH/$2$i ./$i
+  echo -n "▓"
 done
+
+echo -e "\nTHE END"
