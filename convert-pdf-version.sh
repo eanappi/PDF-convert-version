86d7d83ac3e32d655b3526d49daf8aebce87ce0c
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-07-31 11:01:57 -0300

    FIX - Progress information

--- convert-pdf-version.sh
+++ convert-pdf-version.sh
@@ -6,7 +6,14 @@ cd $1
 for i in *
 do
   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$FILEPATH/$2$i ./$i
+
   echo -n "▓"
+
+  # for symbols in \| / - \\ \| / -
+  # do 
+  #   echo -en "\r$i ($symbols)";
+  #   sleep .1
+  # done
 done
 
 echo -e "\nTHE END"
--- used_files/file-generator.sh
+++ used_files/file-generator.sh
@@ -3,7 +3,13 @@
 for i in $(eval echo "{01..$3}")
 do
   cp $1 $2/20045574327-$i.pdf
+
   echo -n "▓"
+
+  # for i in \| / \\ \|
+  # do 
+  #   echo -en "\r$i"; 
+  # done
 done
 
 echo -e "\nTHE END"
