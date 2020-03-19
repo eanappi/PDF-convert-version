200814d00410d9205bae38c91c0f0000f15aa846
Ernesto A. Nappi <eanappi@gmail.com>
2020-03-19 11:46:28 -0300

    Update convert-pdf-version.sh

--- convert-pdf-version.sh
+++ convert-pdf-version.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-if [[ $# -lt 2 ]]; then
-  echo "Faltan argumentos"
-else
-  cd "$1"
+  cd "source"
 
   for i in *
   do
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$2/$i" "$i"
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="../convert/$i" "$i"
 
     echo -n "▓"
   done
 
   echo -e "\n >>>> THE END <<<<"
-fi
